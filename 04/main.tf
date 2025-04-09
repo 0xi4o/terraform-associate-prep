f2b1da9895ce07935a43f1ea5aeaf6ecfc6d872a
Ilango <rajagopalilango@gmail.com>
2025-04-09 08:24:39 +0530

    Basic provider and resource setup

---
+++ 04/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.94.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "t2micro" {
+  ami           = "ami-0e35ddab05955cf57"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyTestServer"
+  }
+}
